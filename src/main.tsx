0ea19a680a7c067ee52ba356e15df499161d188a
kiran <kirankumarka43@gmail.com>
2025-12-31 00:39:55 +0530

    added new fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+// import "@fontsource/syne/400.css"
+// import "@fontsource/syne/600.css"
+import "@fontsource/syne/700.css"
+import "@fontsource/syne-mono/latin-ext-400.css"
+// import "@fontsource/syne/800.css"
 import './index.css'
 import App from './App.tsx'
 
